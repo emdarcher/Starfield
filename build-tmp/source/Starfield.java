b3b9f96130c7878c7ba910a3bd594b38995465b8
Ethan Durrant <emd519@yahoo.com>
2014-10-25 10:36:12 -0700

    added Jumbo class

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -20,83 +20,91 @@ Particle [] parties;
 //your code here
 public void setup()
 {
-	//your code here
-	size(255,255);
-	init_parties();
+    //your code here
+    size(255,255);
+    init_parties();
 
-	frameRate(10);
-	//party = new NormalParticle();
+    frameRate(10);
+    //party = new NormalParticle();
 }
 public void draw()
 {
-	background(128);
-	//your code here
-	for(int i = parties.length-1;i>=0;i--){
-		parties[i].move();
-		parties[i].show();
-	}
+    background(128);
+    //your code here
+    for(int i = parties.length-1;i>=0;i--){
+        parties[i].move();
+        parties[i].show();
+    }
 }
 class NormalParticle implements Particle
 {
-	//your code here
-	private double X_pos,Y_pos,Angle_val,Speed_val;
-	private int P_color;
-	NormalParticle(){
-		X_pos=(double)(width>>1);
-		Y_pos=(double)(height>>1);
-		Angle_val = Math.random()*(Math.PI*2);
-		Speed_val = 10;
-	}
-	public void move(){
-		X_pos += Math.cos(Angle_val)*Speed_val;
-		Y_pos += Math.sin(Angle_val)*Speed_val;
-	}
-	public void show(){
-		fill(P_color);
-		ellipse((int)X_pos,(int)Y_pos,8,8);
-	}
+    //your code here
+    protected double X_pos,Y_pos,Angle_val,Speed_val;
+    protected int P_color;
+    NormalParticle(){
+        X_pos=(double)(width>>1);
+        Y_pos=(double)(height>>1);
+        Angle_val = Math.random()*(Math.PI*2);
+        Speed_val = 10;
+    }
+    public void move(){
+        X_pos += Math.cos(Angle_val)*Speed_val;
+        Y_pos += Math.sin(Angle_val)*Speed_val;
+    }
+    public void show(){
+        fill(P_color);
+        ellipse((int)X_pos,(int)Y_pos,8,8);
+    }
 }
 interface Particle
 {
-	//your code here
-	public void move();
-	public void show();
+    //your code here
+    public void move();
+    public void show();
 }
 class OddballParticle implements Particle
 {
-	//your code here
-	//your code here
-	private double X_pos,Y_pos,Angle_val,Speed_val;
-	private int P_color;
-	int offset_val;
-	OddballParticle(){
-		X_pos=(double)(width>>1);
-		Y_pos=(double)(height>>1);
-		Angle_val = Math.random()*(Math.PI*2);
-		Speed_val = 10;
-		P_color = 0xff429839;
-		offset_val = 16;
-	}
-	public void move(){
-		X_pos += (Math.cos(Angle_val)*Speed_val)+(Math.random()*offset_val);
-		Y_pos += (Math.sin(Angle_val)*Speed_val)+(Math.random()*offset_val);
-	}
-	public void show(){
-		fill(P_color);
-		ellipse((int)X_pos,(int)Y_pos,10,10);
-	}
+    //your code here
+    //your code here
+    private double X_pos,Y_pos,Angle_val,Speed_val;
+    private int P_color;
+    int offset_val;
+    OddballParticle(){
+        X_pos=(double)(width>>1);
+        Y_pos=(double)(height>>1);
+        Angle_val = Math.random()*(Math.PI*2);
+        Speed_val = 10;
+        P_color = 0xff429839;
+        offset_val = 16;
+    }
+    public void move(){
+        X_pos += (Math.cos(Angle_val)*Speed_val)+(Math.random()*offset_val);
+        Y_pos += (Math.sin(Angle_val)*Speed_val)+(Math.random()*offset_val);
+    }
+    public void show(){
+        fill(P_color);
+        ellipse((int)X_pos,(int)Y_pos,10,10);
+    }
+}
+class Jumbo extends NormalParticle {
+    public void show(){
+        fill(255);
+        ellipse((int)X_pos,(int)Y_pos,32,32);
+    }
+
 }
 public void mousePressed(){
-	background(128);
-	init_parties();
-	redraw();
+    background(128);
+    init_parties();
+    redraw();
 }
 public void init_parties(){
-	parties = new Particle[NUM_PARTICLES];
-	parties[0] = new OddballParticle();
-	for(int i = 1;i<parties.length;i++){
-		parties[i] = new NormalParticle();
-	}
+    parties = new Particle[NUM_PARTICLES];
+    parties[0] = new OddballParticle();
+    parties[1] = new Jumbo();
+    for(int i = 2;i<parties.length;i++){
+        parties[i] = new NormalParticle();
+    }
 }
 
   static public void main(String[] passedArgs) {
